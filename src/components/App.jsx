7e9acee283b0a2534fc2d641f55e706659316e09
sankait36 <sankait36@hotmail.com>
2019-09-15 15:16:50 -0500

    Added status bar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
+import StatusBar from './StatusBar';
 
 import '../../styles/App.scss';
-import { version } from '../../package.json';
-
-const { ipcRenderer } = window.require('electron');
-
 
 class App extends Component {
   constructor() {
@@ -18,28 +15,27 @@ class App extends Component {
   }
 
   render() {
-    ipcRenderer.on('message', (event, text) => {
-      console.log(text);
-    });
     return (
-      <div className="container custom-container">
-        <h2>This is version: {version}</h2>
-        <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-          </div>
-          <div className="form-group">
-            <label htmlFor="exampleInputPassword1">Password</label>
-            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-          </div>
-          <div className="form-group form-check">
-            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-            <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-          </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
+      <div>
+        <div className="container custom-container">
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="exampleInputEmail1">Email address</label>
+              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+              <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div className="form-group">
+              <label htmlFor="exampleInputPassword1">Password</label>
+              <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+            </div>
+            <div className="form-group form-check">
+              <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+              <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+        </div>
+        <StatusBar />
       </div>
     );
   }
